d0f591172507cdae63cb112a157befa898b99905
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2020-06-22 20:18:15 +0200

    Update zzalias.sh

--- zzalias.sh
+++ zzalias.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 alias zzcd="source /usr/local/turbolab.it/zzalias/zzcd.sh"
 alias zzuntar="tar -zxvf $1"
-alias zzsudoweb="sudo -u www-data -H bash"
+alias zzweb="sudo -u www-data -H bash"
 alias zzsshconfig="sudo nano $HOME/.ssh/config"
 alias zzhosts="sudo nano /etc/hosts"
 alias zzalias="sudo nano $HOME/.bash_aliases && sudo chmod ug=rwx $HOME/.bash_aliases && source $HOME/.bash_aliases"
