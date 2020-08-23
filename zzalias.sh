5ab4fc882c92dc0689c696530a9b8c22addd011d
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2020-08-23 17:47:19 +0200

    Update zzalias.sh

--- zzalias.sh
+++ zzalias.sh
@@ -6,9 +6,10 @@ alias zzsshconfig="sudo nano $HOME/.ssh/config"
 alias zzhosts="sudo nano /etc/hosts"
 alias zzalias="sudo nano $HOME/.bash_aliases && sudo chmod ug=rwx $HOME/.bash_aliases && source $HOME/.bash_aliases"
 alias zzspacehog="du -hs * | sort -rh | head -5"
+alias zzcountfiles="find . -type f | wc -l"
 alias zzsize="sudo du -sh $1"
 alias zznetstat="sudo netstat -tuanp"
 alias zzclients="netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -n"
 alias zzphpsetcli="sudo update-alternatives --set php /usr/bin/php" $1
 alias zzsiege="siege -b -v -r 1 -c 50" $1
-alias zzphp-fpm-count="ps aux | grep \"php-fpm: pool\" | wc -l"
+alias zzcountfpm="ps aux | grep \"php-fpm: pool\" | wc -l"
