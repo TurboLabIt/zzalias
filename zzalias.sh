31903e52c32c23d56b1c85fd24cb17882404f374
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-01-30 11:17:33 +0100

    Update zzalias.sh

--- zzalias.sh
+++ zzalias.sh
@@ -8,7 +8,7 @@ alias zzalias="sudo nano $HOME/.bash_aliases && sudo chmod ug=rwx $HOME/.bash_al
 alias zzspacehog="du -hs * | sort -rh | head -5"
 alias zzcountfiles="find . -type f | wc -l"
 alias zzsize="sudo du -sh $1"
-alias zznetstat="sudo netstat -tuanp"
+alias zzports="sudo netstat -tuanp"
 alias zzclients="netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -n"
 alias zzphpsetcli="sudo update-alternatives --set php /usr/bin/php" $1
 alias zzsiege="siege -b -v -r 1 -c 50" $1
