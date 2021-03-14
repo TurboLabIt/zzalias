0d6ecd0235c8f70c21443481f5d9fc1784a7accd
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-03-14 17:25:06 +0100

    Update zzalias.sh

--- zzalias.sh
+++ zzalias.sh
@@ -17,6 +17,7 @@ alias zzip="curl http://ipinfo.io/ip"
 alias zzreboot="shutdown -r +10"
 alias zzmirrorto="rsync --archive --compress --delete --partial --progress --verbose ." $1
 alias zzdf="df -h | grep -v loop | grep -v tmp | grep -v udev"
+alias zznmap="nmap -T4 -A -p- -v" $1
 
 
 
