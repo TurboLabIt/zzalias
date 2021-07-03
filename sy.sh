6c059b867ec6b8e7e5b9a30e362e553dbb7c7233
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-07-03 22:03:36 +0200

    Update sy.sh

--- sy.sh
+++ sy.sh
@@ -16,38 +16,40 @@ echo $(pwd)
 
 if [ "$1" == "start" ]; then
 
-	symfony proxy:stop
-	symfony proxy:start
+	XDEBUG_MODE=off symfony proxy:stop
+	XDEBUG_MODE=off symfony proxy:start
 
-	symfony server:stop
-	symfony server:start -d
+	XDEBUG_MODE=off symfony server:stop
+	XDEBUG_MODE=off symfony server:start -d
 
 elif [ "$1" == "stop" ]; then
 
-	symfony proxy:stop
-	symfony server:stop
+	XDEBUG_MODE=off symfony proxy:stop
+	XDEBUG_MODE=off symfony server:stop
 
 elif [ "$1" == "migrate" ]; then
 
-	symfony console doctrine:migrations:migrate --no-interaction
+	XDEBUG_MODE=off symfony console doctrine:migrations:migrate --no-interaction
 	
 elif [ "$1" == "fixt" ]; then
 
-	symfony console doctrine:fixtures:load --no-interaction
+	XDEBUG_MODE=off symfony console doctrine:fixtures:load --no-interaction
 	
 elif [ "$1" == "dropdb" ]; then
 
-	symfony console --env=dev doctrine:schema:drop --force && symfony console doctrine:migrations:migrate --no-interaction
+	XDEBUG_MODE=off symfony console --env=dev doctrine:schema:drop --force && symfony console doctrine:migrations:migrate --no-interaction
 
 elif [ "$1" == "test" ]; then
 
-	php bin/phpunit
+	XDEBUG_MODE=off php bin/phpunit
 
 elif [ "$1" == "cache" ]; then
 
-	sudo zzxdebug off
-	symfony console cache:clear --no-optional-warmers
-	sudo zzxdebug on
+	XDEBUG_MODE=off symfony console cache:clear --no-optional-warmers
+	
+elif [ "$1" == "comp" ]; then
+
+	XDEBUG_MODE=off symfony composer "$@"
 else
 
 	symfony console "$@"
