d32cf704e58faf256bb187112b155a708db34c17
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-12-12 18:13:04 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,14 +19,14 @@ mkdir -p "/etc/turbolab.it/"
 ## Install/update
 echo ""
 if [ ! -d "$INSTALL_DIR" ]; then
-	echo "Installing..."
-	echo "-------------"
-	mkdir -p "$INSTALL_DIR_PARENT"
-	cd "$INSTALL_DIR_PARENT"
-	git clone https://github.com/TurboLabIt/${SCRIPT_NAME}.git
+  echo "Installing..."
+  echo "-------------"
+  mkdir -p "$INSTALL_DIR_PARENT"
+  cd "$INSTALL_DIR_PARENT"
+  git clone https://github.com/TurboLabIt/${SCRIPT_NAME}.git
 else
-	echo "Updating..."
-	echo "----------"
+  echo "Updating..."
+  echo "----------"
 fi
 
 ## Fetch & pull new code
@@ -36,71 +36,72 @@ git pull
 
 if [ ! -f "/etc/turbolab.it/zzcd_bookmarks.sh" ]; then
 
-	echo "## This is your zzcd file! Edit away and make it truly yours!" > "/etc/turbolab.it/zzcd_bookmarks.sh"
-	echo "#"  >> "/etc/turbolab.it/zzcd_bookmarks.sh"
-	echo "# Pro-tip: open this file quickly with one command: zzcd edit" >> "/etc/turbolab.it/zzcd_bookmarks.sh"
-	echo "#"  >> "/etc/turbolab.it/zzcd_bookmarks.sh"
-	CONFIG_CONTENT=$(cat "${INSTALL_DIR}zzcd_bookmarks.default.sh" | grep -v '#')
-	echo "$CONFIG_CONTENT" >> "/etc/turbolab.it/zzcd_bookmarks.sh"
+  echo "## This is your zzcd file! Edit away and make it truly yours!" > "/etc/turbolab.it/zzcd_bookmarks.sh"
+  echo "#"  >> "/etc/turbolab.it/zzcd_bookmarks.sh"
+  echo "# Pro-tip: open this file quickly with one command: zzcd edit" >> "/etc/turbolab.it/zzcd_bookmarks.sh"
+  echo "#"  >> "/etc/turbolab.it/zzcd_bookmarks.sh"
+  CONFIG_CONTENT=$(cat "${INSTALL_DIR}zzcd_bookmarks.default.sh" | grep -v '#')
+  echo "$CONFIG_CONTENT" >> "/etc/turbolab.it/zzcd_bookmarks.sh"
 fi
 
 
 ## Command: ZZGIT
 if [ ! -e "/usr/bin/zzgit" ]; then
 
-	ln -s ${INSTALL_DIR}zzgit.sh /usr/bin/zzgit
+  ln -s ${INSTALL_DIR}zzgit.sh /usr/bin/zzgit
 fi
 
 
 ## Command: ZZWS
 if [ ! -e "/usr/bin/zzws" ]; then
 
-	ln -s ${INSTALL_DIR}zzws.sh /usr/bin/zzws
+  ln -s ${INSTALL_DIR}zzws.sh /usr/bin/zzws
 fi
 
 
 ## Command: ZZXDEBUG
 if [ ! -e "/usr/bin/zzxdebug" ]; then
 
-	ln -s ${INSTALL_DIR}zzxdebug.sh /usr/bin/zzxdebug
+  ln -s ${INSTALL_DIR}zzxdebug.sh /usr/bin/zzxdebug
 fi
 
 
 ## Command: sy
 if [ ! -e "/usr/bin/sy" ]; then
 
-	ln -s ${INSTALL_DIR}sy.sh /usr/bin/sy
+  ln -s ${INSTALL_DIR}sy.sh /usr/bin/sy
 fi
 
 
 ## Other one-liners as aliases
-if [ "$(logname)" == "root" ]; then
+if [ "$(logname)" != "root" ]; then
 
-	ALIASES_FILE=/root/.bash_aliases
-	
+  
+  ALIASES_FILE=/home/$(logname)/.bash_aliases
+  
 else
 
-	ALIASES_FILE=/home/$(logname)/.bash_aliases
+  ALIASES_FILE=/root/.bash_aliases
 fi
 
 
 
 if [ ! -f "$ALIASES_FILE" ]; then
 
-	echo "#!/usr/bin/env bash" >> "$ALIASES_FILE"
+  echo "#!/usr/bin/env bash" >> "$ALIASES_FILE"
 fi
 
 
 if grep -q zzalias "$ALIASES_FILE"; then
 
-	echo "$ALIASES_FILE was already patched"
-	
+  echo "$ALIASES_FILE was already patched"
+  
 else
 
-	echo "source ${INSTALL_DIR}${SCRIPT_NAME}.sh" >> "$ALIASES_FILE"
-	echo "$ALIASES_FILE has now been patched"
+  echo "source ${INSTALL_DIR}${SCRIPT_NAME}.sh" >> "$ALIASES_FILE"
+  echo "$ALIASES_FILE has now been patched"
 fi
-	
+  
 chmod ug=rwx,o=rx "$ALIASES_FILE"
 source "$ALIASES_FILE"
 
