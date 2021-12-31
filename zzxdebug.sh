4b45d6aecaf5d5b42021c012f61a0dd87e5e07ad
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-12-31 11:17:03 +0100

    Update zzxdebug.sh

--- zzxdebug.sh
+++ zzxdebug.sh
@@ -3,20 +3,22 @@
 ## Title printing function
 function printTitle {
 
-    echo ""
-    echo "$1"
-    printf '%0.s-' $(seq 1 ${#1})
-    echo ""
+  echo ""
+  echo -e "\e[1;44m${1}\e[0m"
+  printf '%0.s-' $(seq 1 ${#1})
+  echo ""
 }
 
-printTitle "zzxdebug"
+echo ""
+echo -e "\e[1;41m 🐛 ZZXDEBUG IS RUNNING \e[0m"
 
 
 for PHP_VERSION_DIR in `find /etc/php -maxdepth 1 -mindepth 1 -type d`
 do
 
 	PHP_VERSION_DIR=${PHP_VERSION_DIR}/
-	printTitle "Working on $PHP_VERSION_DIR"
+	PHP_VERSION=$(basename $PHP_VERSION_DIR)
+	printTitle "Working on PHP ${PHP_VERSION} - $PHP_VERSION_DIR"
 
 	for FPM_CLI_STRING in "cli" "fpm"
 	do
@@ -44,10 +46,14 @@ do
 				sudo mv ${DISABLED_DIR}* "${FPM_CLI_DIR}conf.d/" 2>/dev/null
 				sudo rm -rf "${DISABLED_DIR}"
 
+        if [ "${FPM_CLI_STRING}" == "fpm" ]; then
+          echo -e "\e[1;33m Restarting php${PHP_VERSION}-fpm... \e[0m"
+          sudo service php${PHP_VERSION}-fpm restart
+        fi
 			fi
-	
+
 		else
-			
+
 			if [ -d  "$DISABLED_DIR" ]; then
 
 				echo " ->zzxdebug-disabled found, skipping"
@@ -57,14 +63,18 @@ do
 				sudo mkdir "${DISABLED_DIR}"
 				sudo mv ${FPM_CLI_DIR}conf.d/*xdebug* "${DISABLED_DIR}" 2>/dev/null
 
-			fi	
+        if [ "${FPM_CLI_STRING}" == "fpm" ]; then
+          echo -e "\e[1;33m Restarting php${PHP_VERSION}-fpm... \e[0m"
+          sudo service php${PHP_VERSION}-fpm restart
+        fi
+			fi
+
 		fi
 
 	done
-	
+
 done
 
 
 printTitle "Operation completed"
 echo ""
-
