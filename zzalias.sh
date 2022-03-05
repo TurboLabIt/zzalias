d7249ed5bb7b4ca5d110c076b54fad6fe875d641
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-03-05 10:55:33 +0100

    Update zzalias.sh

--- zzalias.sh
+++ zzalias.sh
@@ -11,7 +11,6 @@ alias zzalias="sudo nano $HOME/.bash_aliases && sudo chmod ug=rwx $HOME/.bash_al
 alias zzspacehog="du -hs * | sort -rh | head -5"
 alias zzcountfiles="find . -type f | wc -l"
 alias zzsize="sudo du -sh"
-alias zzports="sudo netstat -putan"
 alias zzphpsetcli="sudo update-alternatives --set php /usr/bin/php"
 alias zzsiege="siege -b -v -r 1 -c 50"
 alias zzcountfpm="ps aux | grep \"php-fpm: pool\" | wc -l"
@@ -20,7 +19,7 @@ alias zzreboot="shutdown -r +10"
 alias zzdf="df -h | grep -v loop | grep -v tmp | grep -v udev | grep -v /boot/efi"
 alias zznmap="nmap -T4 -A -p- -v"
 alias zzsetrtc="sudo timedatectl set-local-rtc 1 --adjust-system-clock && timedatectl"
-alias zznetstat="sudo netstat -antup"
+alias zzports="sudo netstat -putan"
 
 function zzclients()
 {
