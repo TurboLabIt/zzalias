d2d1aece390c3c5d1cdfa145d0ebb58a4046e65f
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-03-20 10:33:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,6 @@ echo ""
 ## Script name
 SCRIPT_NAME=zzalias
 
-## Pre-requisites
-apt update
-apt install git pv -y
-
 ## Install directory
 WORKING_DIR_ORIGINAL="$(pwd)"
 INSTALL_DIR_PARENT="/usr/local/turbolab.it/"
@@ -19,20 +15,26 @@ mkdir -p "/etc/turbolab.it/"
 ## Install/update
 echo ""
 if [ ! -d "$INSTALL_DIR" ]; then
+
+  ## Pre-requisites
+  apt update && apt install git pv -y
+
   echo "Installing..."
   echo "-------------"
   mkdir -p "$INSTALL_DIR_PARENT"
   cd "$INSTALL_DIR_PARENT"
   git clone https://github.com/TurboLabIt/${SCRIPT_NAME}.git
+  
 else
+
   echo "Updating..."
   echo "----------"
+  
 fi
 
 ## Fetch & pull new code
 cd "$INSTALL_DIR"
 git pull
-
 
 if [ ! -f "/etc/turbolab.it/zzcd_bookmarks.sh" ]; then
 
@@ -44,36 +46,30 @@ if [ ! -f "/etc/turbolab.it/zzcd_bookmarks.sh" ]; then
   echo "$CONFIG_CONTENT" >> "/etc/turbolab.it/zzcd_bookmarks.sh"
 fi
 
-
 ## Command: ZZALIAS - Allows zzalias do be loaded manually
 if [ ! -e "/usr/bin/zzalias" ]; then
   ln -s ${INSTALL_DIR}zzalias.sh /usr/bin/zzalias
 fi
-
 
 ## Command: ZZGIT
 if [ ! -e "/usr/bin/zzgit" ]; then
   ln -s ${INSTALL_DIR}zzgit.sh /usr/bin/zzgit
 fi
 
-
 ## Command: ZZWS
 if [ ! -e "/usr/bin/zzws" ]; then
   ln -s ${INSTALL_DIR}zzws.sh /usr/bin/zzws
 fi
-
 
 ## Command: ZZXDEBUG
 if [ ! -e "/usr/bin/zzxdebug" ]; then
   ln -s ${INSTALL_DIR}zzxdebug.sh /usr/bin/zzxdebug
 fi
 
-
 ## Command: sy
 if [ ! -e "/usr/bin/sy" ]; then
   ln -s ${INSTALL_DIR}sy.sh /usr/bin/sy
 fi
-
 
 ## Command: dock
 if [ ! -e "/usr/bin/zzdock" ]; then
@@ -93,13 +89,12 @@ if [ "$(logname)" != "root" ]; then
 else
 
   ALIASES_FILE=/root/.bash_aliases
+  
 fi
-
 
 if [ ! -f "$ALIASES_FILE" ]; then
   echo "#!/usr/bin/env bash" >> "$ALIASES_FILE"
 fi
-
 
 if grep -q zzalias "$ALIASES_FILE"; then
 
@@ -114,8 +109,10 @@ fi
 chmod ug=rwx,o=rx "$ALIASES_FILE"
 source "$ALIASES_FILE"
 
-
 ## Restore working directory
 cd $WORKING_DIR_ORIGINAL
 
-echo "You should customize your zzcd now. Just run: zzcd edit"
+echo ""
+echo "Setup completed!"
+echo "----------------"
+echo "See https://github.com/TurboLabIt/${SCRIPT_NAME} for the quickstart guide."
