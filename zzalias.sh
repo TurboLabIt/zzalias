5680962683e84b9fc6ea9d14495efa634a5ab4aa
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-05-30 18:01:44 +0200

    Update zzalias.sh

--- zzalias.sh
+++ zzalias.sh
@@ -47,3 +47,12 @@ function zzmirrorfrom()
 {
   rsync --archive --compress --delete --partial --progress --verbose $1 .
 }
+
+
+function zzoom()
+{
+journalctl --list-boots | \
+    awk '{ print $1 }' | \
+    xargs -I{} journalctl --utc --no-pager -b {} -kqg 'killed process' -o verbose --output-fields=MESSAGE
+
+}
