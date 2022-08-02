87f35cbec9086384ddfd09a556004753784207ca
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-08-02 17:24:34 +0200

    Update zzalias.sh

--- zzalias.sh
+++ zzalias.sh
@@ -53,7 +53,7 @@ function zzmirrorfrom()
 
 function zzoom()
 {
-journalctl --list-boots | \
+sudo journalctl --list-boots | \
     awk '{ print $1 }' | \
     xargs -I{} journalctl --utc --no-pager -b {} -kqg 'killed process' -o verbose --output-fields=MESSAGE
 
