d03bc759a750b9fba1483f2ad86a7b8da5d1e249
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-09 00:47:22 +0100

    Update zzws.sh

--- zzws.sh
+++ zzws.sh
@@ -15,7 +15,7 @@ fi
 ## bash-fx is ready
 
 
-fxHeader "♻️ Web Stack Restarter"
+fxHeader "🏄‍♂️ Webstackup Web Services Manager (zzws)"
 
 fxTitle "Testing NGINX config..."
 if [ ! -z $(command -v nginx) ]; then
